1e84c4062bd836e093f29d205458101ba3e3956f
suraj vingataredy <surajv4.0@gmail.com>
2018-11-22 18:39:41 +0100

    end of the day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+response = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+list = JSON.parse(response) # JSON parse
+ingredients = list["drinks"] # On obtient un array de 160 elements
+puts 'Creating restaurants...'
+ingredients.each do |i|
+  i.each do |key, value|
+  Ingredient.create!(name: value)
+  end
+end
+
+puts 'Finished!'
