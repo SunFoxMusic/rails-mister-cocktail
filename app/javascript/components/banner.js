ec85f1d8eda29763d51085881921f74fe6932c39
suraj vingataredy <surajv4.0@gmail.com>
2018-11-23 16:19:44 +0100

    added some style again

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Welcome To Mister 'B' Cocktails", "Have fun !"],
+    strings: ["Welcome To Madame 'B' cocktails", "Have fun !"],
     typeSpeed: 50,
     loop: true
   });
